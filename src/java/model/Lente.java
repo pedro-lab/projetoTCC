43b0a27fdd091f0721eac0cde5cac9b4e4caab05
pedro <pedro@DESKTOP-828S66G>
2023-10-25 18:21:36 -0300

    Criação e definicao dos atributos da classe Lente

---
+++ src/java/model/Lente.java
@@ -0,0 +1,21 @@
+package model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Lente {
+    
+    private int idLente;
+    private String nome;
+    private String modelo;
+    private Double preco;
+    
+}
