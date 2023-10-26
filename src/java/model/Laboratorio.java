65a3724a28e9987aaebf0d38a96f3e8befb0ce01
pedro <pedlsjunior12@gmail.com>
2023-10-26 00:47:27 -0300

    Finalizacao da classe LaboratorioDAO

--- src/java/model/Laboratorio.java
+++ src/java/model/Laboratorio.java
@@ -1,5 +1,16 @@
 package model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Laboratorio {
     
     private int idLaboratorio;
@@ -7,5 +18,6 @@ public class Laboratorio {
     private String endereco;
     private String telefone;
     private String email;
+    private  int status; 
     
 }
