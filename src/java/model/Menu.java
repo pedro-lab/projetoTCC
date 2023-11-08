77a11f0248b0f71e3a05a23812f5b280fbab291f
pedro Lopes <pedlsjunior12@gmail.com>
2023-11-08 09:44:25 -0300

    Conclusao da classe Menu do pacote model

--- src/java/model/Menu.java
+++ src/java/model/Menu.java
@@ -1,5 +1,22 @@
 package model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Menu {
+    
+    private int idMenu;
+    private String nome;
+    private String link;
+    private int exibir;
+    private int status;
     
 }
