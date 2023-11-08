af7d80af6c778d574c4b2f766a99c81e755f0e69
pedro Lopes <pedlsjunior12@gmail.com>
2023-11-08 09:51:42 -0300

    Conclusao da Classe Perfil do pacote model

--- src/java/model/Perfil.java
+++ src/java/model/Perfil.java
@@ -1,5 +1,22 @@
 package model;
 
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
 public class Perfil {
+    private int idPerfil;
+    private String nome;
+    private Date dataCadastro;
+    private int status;
     
 }
