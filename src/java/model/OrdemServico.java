7532346907bbe9816a844cfdd246734e248ced39
pedro Lopes <pedlsjunior12@gmail.com>
2023-11-08 22:02:18 -0300

    Conclusao da Classse OrdemServico do pacote model

--- src/java/model/OrdemServico.java
+++ src/java/model/OrdemServico.java
@@ -1,7 +1,28 @@
 package model;
 
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class OrdemServico {
     
-    
+    private int idOs;
+    private Date dataSolicitacao;
+    private Date dataEntrega;
+    private Date vencimento;
+    private String statusEntrega;
+    private Usuario usuario;
+    private Laboratorio laboratorio;
+    private Lente lente;
+    private Cliente cliente;
+    private int status;
     
 }
