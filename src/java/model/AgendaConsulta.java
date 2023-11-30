3837e85d3d1ae0c066a7c9fbf5ced6f92e2a6bd4
pedro Lopes <pedlsjunior12@gmail.com>
2023-11-30 15:16:04 -0300

    Criacao da classe AgendaConsulta do pacote model

---
+++ src/java/model/AgendaConsulta.java
@@ -0,0 +1,17 @@
+package model;
+
+import java.util.Date;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class AgendaConsulta {
+    
+}
